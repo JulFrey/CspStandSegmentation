599eca27d8f23f34df17c2e0f4dcce78e6b69d8e
Julian Frey <45684330+JulFrey@users.noreply.github.com>
2024-03-12 15:05:51 +0100

    Clarify documentation and correct spelling

--- R/2021-11-12_A1_JF_csp_cost_functions.R
+++ R/2021-11-12_A1_JF_csp_cost_functions.R
@@ -12,14 +12,14 @@ invisible(lapply(c('lidR','TreeLS', 'dbscan', 'igraph', 'foreach'), require, cha
 #' it adds geometric features like Curvature, Linearity, Planarity, Sphericity,
 #' Anisotrophy and Verticlity to the points itself.
 #'
-#' Details to the metrics can be found in: \ Hackel, T., Wegner, J.D. &
+#' Details of the metrics can be found in: \ Hackel, T., Wegner, J.D. &
 #' Schindler, K. (2016) Contour Detection in Unstructured 3D Point Clouds. In
 #' 2016 IEEE Conference on Computer Vision and Pattern Recognition (CVPR).
 #' Presented at the 2016 IEEE Conference on Computer Vision and Pattern
 #' Recognition (CVPR), IEEE, Las Vegas, NV, USA, pp. 1610–1618.
 #'
 #' @param las A LAS object (see lidR::LAS)
-#' @param k the k neerest neighbors to use for the eigenvalue calculation
+#' @param k the k nearest neighbors to use for the eigenvalue calculation
 #' @param n_cores The number of CPU cores to use
 #' @return The function returns a single LAS object with the geometric features
 #' attached to it in the LAS@data section.
@@ -58,7 +58,7 @@ add_geometry <- function(las, k = 10, n_cores = 1) {
 #'
 #' Returns a las element with XYZ-coordinates as the voxel center and
 #' X_gr,Y_gr,Z_gr as the center of gravity (mean point coordinates) as well as
-#' all other numeric collumns voxel mean values with their original name.
+#' all other numeric columns voxel mean values with their original name.
 #'
 #' @param las a lidR::LAS element
 #' @param res voxel resolution in meter
@@ -78,7 +78,7 @@ add_geometry <- function(las, k = 10, n_cores = 1) {
 #' @export voxelize_points_mean_attributes
 voxelize_points_mean_attributes <- function(las, res) {
 
-  # checking resolution input validity
+  # Checking resolution input validity
   if (length(res) == 1L) {
     res <- c(res, res)
   } else if (length(res) > 2L) {
@@ -106,10 +106,10 @@ voxelize_points_mean_attributes <- function(las, res) {
 #' Add voxel coordinates to a las file
 #'
 #' Adds the collums x_vox, y_vox and z_vox in the given ressolution to the las
-#' element. This is convinient if informations have been derived in voxel space
+#' element. This is convenient if information has been derived in voxel space
 #' and these should be attached to the original points.
 #'
-#' Voxel coordinates derived with this function are identical as those derived
+#' Voxel coordinates derived with this function are identical to those derived
 #' by lidR::voxelize.
 #'
 #' @param las an element of lidR::LAS class
@@ -146,7 +146,7 @@ add_voxel_coordinates <- function(las, res) {
 
 #' Add all las_attributes from las@data to the header of a las element
 #'
-#' The helper function adds all headings from las@data which are nor part of
+#' The helper function adds all headings from las@data which are not part of
 #' lidR:::LASATTRIBUTES to the las header using lidR::add_lasattribute. Only
 #' attributes that are included in the header got saved when using
 #' lidR::writeLAS, this is a convenient way to add them.
@@ -170,7 +170,7 @@ add_voxel_coordinates <- function(las, res) {
 #' @export add_las_attributes
 add_las_attributes <- function(las) {
 
-  # add attributes from data table permanently to attributes
+  # Add attributes from data.table permanently to attributes
   names <- names(las@data)
   names <- names[!(names %in% lidR:::LASATTRIBUTES)]
   for (name in names) {
@@ -194,13 +194,13 @@ add_las_attributes <- function(las) {
 #'
 #' @param vox a LAS S4 element with XYZ voxel coordinates in the @data slot.
 #' @param adjacency_df a data.frame with voxel ids (row numbers) in the first
-#' column and a neighboring voxel id in the second column and the weight
+#' column and a neighboring voxel ID in the second column and the weight
 #' (distance) in the third column. Might be generated using the dbscan::frNN
 #' function (which requires reshaping the data).
 #' @param seeds seed points for tree positions.
 #' @param v_w,l_w,s_w weights for verticality, linearity spericity see
 #' \code{\link{csp_cost_segmentation}}
-#' @param N_cores Number of cpu cores for multi-threading
+#' @param N_cores Number of CPU cores for multi-threading
 #' @param Voxel_size Edge length used to create the voxels. This is only
 #' important to gain comparable distance weights on different voxel sizes.
 #' Should be greater than 0.
@@ -231,7 +231,7 @@ comparative_shortest_path <- function(vox = vox, adjacency_df = adjacency_df, se
     igraph::graph_from_data_frame(directed = F) |>
     igraph::simplify()
 
-  # calculate a distance (weight) graph per seed using dijkstra
+  # calculate a distance (weight) graph per seed using Dijkstra
   doParallel::registerDoParallel(cores = N_cores)
   dists_list <- foreach::foreach(
     t = 1:nrow(seeds),
@@ -243,20 +243,20 @@ comparative_shortest_path <- function(vox = vox, adjacency_df = adjacency_df, se
 
   unreachable <- which(sapply(dists_list,function(x) is.character(x[[1]])))
   if(length(unreachable) > 0) {
-    warning('Not all base positions could be reached by the graph. Try a lower resolution or a different approach to find tree base positions. Error messages for the unreachble TreeIDs:')
-    warning(paste0(unreachable , paste(":",paste(dists_list[unreachable]), collaps = " "), "\n"), call. = F)
+    warning('Not all base positions could be reached by the graph. Try a lower resolution or a different approach to find tree base positions. Error messages for the unreachable TreeIDs:')
+    warning(paste0(unreachable , paste(":",paste(dists_list[unreachable]), collapse = " "), "\n"), call. = F)
     seeds <- seeds[-unreachable,]
     dists_list <- dists_list[-unreachable]
     }
 
-  # combine to matrix
+  # Combine to matrix
   dist_matrix <- simplify2array(dists_list)[1,,]
 
   # get seed with minimum distance
   min_matrix <- apply(dist_matrix, 1, which.min)
   min_dist_matrix <- suppressWarnings(apply(dist_matrix, 1, min, na.rm = T))
   min_matrix <- data.table::data.table(PointID = as.integer(igraph::V(vox_graph)$name), TreeID = seeds$TreeID[as.integer(min_matrix)], dist = min_dist_matrix)
-  min_matrix$TreeID[min_dist_matrix == Inf] <- 0 # set SeedIDs 0 for voxels which can't be reached by any seed
+  min_matrix$TreeID[min_dist_matrix == Inf] <- 0 # set SeedIDs 0 for voxels which any seed can't reach
 
   # assign voxels to seeds (minimum cost/distance to trunk)
   vox <- vox |>
@@ -268,36 +268,37 @@ comparative_shortest_path <- function(vox = vox, adjacency_df = adjacency_df, se
 
 # ------------------------------------------------------------------------------
 
-# this is the main function
-# it requires a normalized las point cloud of a forest patch with already calculated geometric features
-# using the add geometry function,
-# a forest inventory as it can be calculated by TreeLS::tlsInventory
+# This is the main function
+# It requires a las point cloud of a forest patch and
+# a forest inventory as it can be calculated by TreeLS::tlsInventory.
+# Preferable geometric features should be computed for the point cloud prior 
+# to the use of this function using 'add_geometry()'.
 
 #' Comparative Shortest Path with cost weighting tree segmentation
 #'
 #' Segments single trees from forest point clouds based on tree positions
-#' (xy-coordinates) provided in the map-argument.
+#' (xyz-coordinates) provided in the map argument.
 #'
 #' The whole point cloud is voxelized in the given resolution and the center of
 #' gravity for the points inside is calculated as voxel coordinate. A graph is
-#' build which connects the voxel-coordinates based on db-scan algorithm. The
-#' distances between the voxel-coordinates is weighted based on geometric
+#' build, which connects the voxel coordinates based on a db-scan algorithm. The
+#' distances between the voxel coordinates is weighted based on geometric
 #' features computed for the points in the voxels. Distances along planar
 #' and/or vertical faces like stems are weighted shorter than distances through
-#' voxels with a high sphericity like leaves and clusters of twigs. This
-#' avoids, that small trees and regrowth takes over points from mature trees.
-#' For every voxel-center the weighted distance in the network is calculated to
-#' all tree-locations from the map-argument. The TreeID of the map argument
+#' voxels with high sphericity like leaves and clusters of twigs. This
+#' avoids small individuals spreading into the upper canopy.
+#' For every voxel center, the weighted distance in the network is calculated to
+#' all tree locations from the map argument. The TreeID of the map argument
 #' with the shortest distance is assigned to the voxel. All points in the point
-#' cloud get the TreeID from their parent voxel.
+#' cloud receive the TreeID from their parent voxel.
 #'
 #' @param las A lidR LAS S4 object.
-#' @param map A TreeLS map object, or a data.frame including the columns
-#' X,Y,Z,TreeID, with X and Y depicting the location of the trees.
+#' @param map A TreeLS map object, or a data.frame, including the columns
+#' X, Y, Z, TreeID, with X and Y depicting the location of the trees.
 #' @param Voxel_size The voxel size (3D resolution) for the routing graph to
-#' determine nearest map location for every point in the point cloud.
-#' @param V_w verticality weight. Since trunks are vertical structures routing
-#' through voxels with high verticality can be rated 'cheaper'. should be a
+#' determine the nearest map location for every point in the point cloud.
+#' @param V_w verticality weight. Since trunks are vertical structures, routing
+#' through voxels with high verticality can be rated 'cheaper.' should be a
 #' number between 0 and 1 with 0 meaning no benefit for more vertical
 #' structures.
 #' @param L_w Linearity weight. Similar to V_w but for linearity, higher
@@ -320,7 +321,7 @@ comparative_shortest_path <- function(vox = vox, adjacency_df = adjacency_df, se
 #' # normalize height
 #' tls <- TreeLS::tlsNormalize(tls)
 #'
-#' # find tree positions as starting point for segmentation
+#' # Find tree positions as starting points for segmentation
 #' map <- TreeLS::treeMap(tls)
 #'
 #' # segment trees
@@ -339,16 +340,16 @@ csp_cost_segmentation <- function(las, map, Voxel_size = 0.3, V_w = 0, L_w = 0, 
     las <- las |>
       lidR::remove_lasattribute('TreeID')
   }
-  # check if geometric features exist in las and copute dummies if not
+  # Check if geometric features exist in las and compute dummies if not
   if(!all(c('Sphericity', 'Linearity', 'Verticality') %in% names(las@data))) {
     warning("no geometric features in las.  V_w, L_w and/or S_w weights will be ignored.Use 'las <- las |> add_gemetry()' prior to calling this function.")
     las <- las |>
-      lidR::add_lasattribute(0,'Sphericity','Sphericity') |>
-      lidR::add_lasattribute(0,'Linearity','Linearity') |>
-      lidR::add_lasattribute(0,'Verticality','Verticality')
+      lidR::add_lasattribute(0, 'Sphericity', 'Sphericity') |>
+      lidR::add_lasattribute(0, 'Linearity', 'Linearity') |>
+      lidR::add_lasattribute(0, 'Verticality', 'Verticality')
   }
 
-  # voxelize las with mean attributes
+  # Voxelize las with mean attributes
   vox <- voxelize_points_mean_attributes(las, res = Voxel_size)
 
   if (typeof(map) == 'S4') {
@@ -360,12 +361,12 @@ csp_cost_segmentation <- function(las, map, Voxel_size = 0.3, V_w = 0, L_w = 0, 
     inv <- map
   }
 
-  # add seeds
+  # Add seeds
   vox <- vox |>
     lidR::add_lasattribute(0, 'TreeID', 'TreeID')
   vox@data <- vox@data[,c('X', 'Y', 'Z', 'X_gr', 'Y_gr', 'Z_gr', 'Sphericity', 'Linearity', 'Verticality', 'TreeID')]
 
-  # lift the starting points if map doesn't have Z values
+  # Lift the starting points if map doesn't have Z values
   if (sum(inv$Z) == 0) {
     inv$Z <- 0.5
   }
@@ -380,46 +381,33 @@ csp_cost_segmentation <- function(las, map, Voxel_size = 0.3, V_w = 0, L_w = 0, 
     cbind(Verticality = 0)
   vox@data <- rbind(vox@data, inv[,c('X', 'Y', 'Z', 'X_gr', 'Y_gr', 'Z_gr', 'Sphericity', 'Linearity', 'Verticality', 'TreeID')])
 
-  # possible seeds
+  # Seed positions
   seed_range <- (nrow(vox@data) - nrow(inv) + 1):nrow(vox@data)
   tree_seeds <- data.frame(SeedID = seed_range, TreeID = vox@data$TreeID[seed_range])
   rm(seed_range)
 
-  # use dbscan to calculate a matrix of neighboring points
+  # Use dbscan to calculate a matrix of neighboring points
   neighborhood_list <- dbscan::frNN(vox@data[,c('X_gr', 'Y_gr', 'Z_gr')], Voxel_size * 2, bucketSize = 22)
   # dbscan::frNN(vox@data[,c('X_gr', 'Y_gr', 'Z_gr')], Voxel_size * 2, bucketSize = 22)  # voxel size * 1.42 (sqrt(1^2 + 1^2)) 1.73
 
-  # the result has to be disentangled we get the adjacent voxel ids first
+  # The result has to be disentangled we get the adjacent voxel IDs first
   adjacency_list <- unlist(neighborhood_list$id)
 
-  # then we grab the origin voxel using cpp
+  # Then we grab the origin voxel using cpp
   adjacency_list_id <- fast_unlist(neighborhood_list$id, length(adjacency_list)) + 1 # +1 because of cpp counting
 
-  # we do the same with the distances
+  # We do the same with the distances
   dists_vec <- fast_unlist_dist(neighborhood_list$dist, length(adjacency_list))
 
-  # compile to a data frame
+  # Compile to a data frame
   adjacency_df <- data.frame(adjacency_list_id,adjacency_list, weight = dists_vec) #, TreeID = vox@data$TreeID[adjacency_list_id]
- #, TreeID = vox@data$TreeID[adjacency_list_idk]
-  # # remove seeds which are outside of the point cloud
-  # exclude_loops <- dbscan::comps(neighborhood_list)
-  # tab_loops <- table(exclude_loops)
-  # exclude_loops <- exclude_loops %in% as.integer(names(tab_loops)[tab_loops > 3])
-  # adjacency_df <- adjacency_df[adjacency_df$adjacency_list_id %in% c(1:nrow(vox))[exclude_loops] & adjacency_df$adjacency_list %in% c(1:nrow(vox))[exclude_loops],]
-  # seeds_in <- tree_seeds$SeedID %in% adjacency_df$adjacency_list_id
-  # # #plot(Y ~ X, data = inv, col = seeds_in+2)
-  # tree_seeds <- tree_seeds[seeds_in,]
-  # clean
   rm(adjacency_list, adjacency_list_id, dists_vec, neighborhood_list)
 
-  # calculate csp including the weights
+  # Calculate CSP, including the weights
   vox2 <- comparative_shortest_path(vox = vox, adjacency_df = adjacency_df, v_w = V_w, l_w = L_w, s_w = S_w, Voxel_size = Voxel_size, N_cores = N_cores, seeds = tree_seeds)
-  #unique(vox2$TreeID) %in% tree_seeds$TreeID
-  #plot(LAS(vox2))
-  #plot(Y ~ X, data = inv, col = inv$TreeID %in% unique(vox2$TreeID)+1)
 
   las <- las |>
-    add_voxel_coordinates(Voxel_size) #|> remove_lasattribute('Radius')
+    add_voxel_coordinates(Voxel_size)
   las@data <- merge(las@data, vox2@data[,c('X', 'Y', 'Z', 'TreeID')], by.x = c('x_vox', 'y_vox', 'z_vox'), by.y = c('X', 'Y', 'Z'))
   las <- add_las_attributes(las)
   return(las)
@@ -427,7 +415,7 @@ csp_cost_segmentation <- function(las, map, Voxel_size = 0.3, V_w = 0, L_w = 0, 
 
 # ------------------------------------------------------------------------------
 
-# own function to calculate tree start points to get rid of TreeLS (since its not on cran)
+# Own function to calculate tree start points to get rid of TreeLS (since it is not on cran)
 
 #' Find stem base position using a density raster approach
 #' @param las an element of lidR::LAS class
@@ -461,7 +449,7 @@ find_base_coordinates_raster <- function(las, res = 0.1, zmin = 0.5, zmax = 2, q
 
 # ------------------------------------------------------------------------------
 
-# own function to calculate tree start points to get rid of TreeLS (since its not on cran)
+# own function to calculate tree start points to get rid of TreeLS (since it is not on cran)
 
 #' Find stem base position using a geometric feature filtering and clustering
 #' approach
@@ -473,8 +461,8 @@ find_base_coordinates_raster <- function(las, res = 0.1, zmin = 0.5, zmax = 2, q
 #' considered a stem point
 #' @param min_planarity minimum planarity >0 & <1 for a point to be considered
 #' a stem point
-#' @param min_cluster_size minimum number of points in cluster to be considered
-#' a tree, if NULL median cluster size is choosen
+#' @param min_cluster_size minimum number of points in the cluster to be considered
+#' a tree, if NULL median cluster size is chosen
 #' @return %% ~Describe the value returned %% If it is a LIST, use %%
 #' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
 #' 'comp2'} %% ...
