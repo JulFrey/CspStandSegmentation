8b9c0fd67ce6a1820ee0c54ad2a448a907beaea2
JulFrey <julian.frey@@posteo.de>
2026-04-02 17:49:40 +0200

    solving check notes

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 if (getRversion() >= "2.15.1")  utils::globalVariables(
   c("X", "Y", "Z", "Znorm", "Zref","Zdiff","tree_pos_height", "Classification", "Planarity", "Verticality", "Cluster", "TreeID", "..cols", ".N", "N", ".SD", "..tree_id_col",".", ".I", ".GRP", ".EACHI", "slice_id",
-    "z_lo", "z_hi", "r", "Height", "ConvexHullArea")
+    "z_lo", "z_hi", "r", "Height", "ConvexHullArea","ID", "color_ID", "R", "G", "B", "color_HEX", "X1", "X2", "X3", "nn_idx", "nn_dist")
 )
